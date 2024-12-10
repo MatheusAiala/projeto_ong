edaee294904ebafa2d7ffaf8ace369b58dacffb6
MatheusAiala <matheussurg10li@gmail.com>
2024-12-10 11:37:50 -0300

    Primeiro commit

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ var elementosDuvidas = document.querySelectorAll(".duvida")
 
 elementosDuvidas.forEach(function(duvida){
     duvida.addEventListener("click", function(){
-        duvida.classList.add("fundo-verde")
+        duvida.classList.toggle("ativa")
     })
 })
